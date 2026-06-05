8ced8d63504df6fd05f3ad97cf1637e8108184a8
BookSlot Dev <bookslot@dev.com>
2026-06-05 10:14:28 +0200

    Refresh identity pages design

--- BookSlot/Program.cs
+++ BookSlot/Program.cs
@@ -52,7 +52,10 @@ builder.Services.AddHttpClient<ResendEmailService>();
 builder.Services.AddScoped<IEmailService, ResendEmailService>();
 builder.Services.AddScoped<IEmailSender, IdentityEmailSender>(); // "Forgot password" emails
 builder.Services.AddScoped<StripeService>();
-builder.Services.AddHostedService<BookingReminderWorker>();
+if (builder.Configuration.GetValue("BookingReminders:Enabled", true))
+{
+    builder.Services.AddHostedService<BookingReminderWorker>();
+}
 builder.Services.AddAiAssistantFeature(builder.Configuration);
 
 var app = builder.Build();
